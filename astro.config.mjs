0b0c5edc03fa18aff519588b9fb1d92203d5ce84
zhuzhu <zhuzhu19870718@gmail.com>
2026-06-26 10:52:30 +0800

    Refactor routing and migrate to official Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://winwinstonecustom.com',
+  site: "https://winwinstonecustom.com",
+
+  integrations: [
+    sitemap(),
+  ],
 });
